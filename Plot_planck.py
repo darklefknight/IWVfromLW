a2c71bd66927a462c72397df057b537b14df7c38
darklefknight <darklefknight@web.de>
2018-01-20 10:34:13 +0100

    planck function normed by max

--- Plot_planck.py
+++ Plot_planck.py
@@ -36,8 +36,8 @@ if __name__ == "__main__":
     micro = 1e-6
 
 
-    sw = m2v((0.38*micro,0.78*micro))
-    lw = m2v((0.78*micro,100*micro))
+    sw = m2v((0.38*micro,5*micro))
+    lw = m2v((5*micro,100*micro))
 
     print(lw,sw)
     sonne_i = np.zeros(100000)
@@ -53,16 +53,16 @@ if __name__ == "__main__":
 
     fig,ax = plt.subplots(nrows=1,ncols=1,figsize=(16,6))
     plt.title("Plancksches Strahlungsgesetz", fontsize=20)
-    ax.loglog(frequency, sonne_i, color="orange",label="Sonne")
-    ax.loglog(frequency,erde_i,color="blue",label="Erde")
+    ax.plot(frequency, np.divide(sonne_i,np.max(sonne_i)), color="orange",label="Sonne")
+    ax.plot(frequency,np.divide(erde_i,np.max(erde_i)),color="blue",label="Erde")
 
     trans = mtransforms.blended_transform_factory(ax.transData, ax.transAxes)
     ax.fill_between(frequency,0,1,where=((lw[1]<frequency) & (frequency<lw[0])),facecolor="red",alpha=0.5,transform=trans, label="langwelliger Bereich")
     ax.fill_between(frequency,0,1,where=((sw[1]<frequency) & (frequency<sw[0])),facecolor="yellow",alpha=0.5,transform=trans, label="kurzwelliger Bereich")
 
 
-    ax.set_xlim(1e7,1e16)
-    ax.set_ylim(1e-20,1e-5)
+    ax.set_xlim(1e7,0.2e16)
+    # ax.set_ylim(1e-20,1e-5)
 
     ax.set_xlabel("Frequenz [Hz]",fontsize=font)
     ax.set_ylabel("Intensität [$W\,m^{-2}\,sr^{-1}\,Hz^{-1}$]",fontsize=font)
@@ -70,6 +70,5 @@ if __name__ == "__main__":
 
     ax.legend(loc="upper left", fontsize=font)
     plt.tight_layout()
-
+    plt.savefig("Planck-funktion.png", dpi=200)
     plt.show()
-    plt.savefig("Planck-funktion.png",dpi=300)
